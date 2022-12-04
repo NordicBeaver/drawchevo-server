fdc152eadb54660c9d2f418917117ba7e7f5fd33
NordicBeaver <nordicbeaver@gmail.com>
2022-12-04 15:18:49 +0200

    Use sockets to update players.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import { createPlayer } from './player';
 import { createRoom, Room } from './room';
 
 const rooms: Room[] = [];
+const connectedPlayers: { playerId: string; socket: Socket }[] = [];
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
 
 // TODO: Limit origins?
 app.use(cors());
@@ -43,10 +48,17 @@ app.post('/room/join', (req, res) => {
     return res.status(404).send('Room not found.');
   }
 
-  const player = createPlayer(body.username);
-  room.players.push(player);
+  const newPlayer = createPlayer(body.username);
+  room.players.push(newPlayer);
 
-  return res.send({ player });
+  room.players.forEach((player) => {
+    const socket = connectedPlayers.find((p) => p.playerId === player.id)?.socket;
+    if (socket) {
+      socket.emit('gameUpdate', { room });
+    }
+  });
+
+  return res.send({ room, player: newPlayer });
 });
 
 app.get('/room/:code', (req, res) => {
@@ -60,7 +72,13 @@ app.get('/room/:code', (req, res) => {
   return res.send({ room });
 });
 
+io.on('connection', (socket) => {
+  socket.on('playerConnect', ({ playerId }: { playerId: string }) => {=
+    connectedPlayers.push({ socket: socket, playerId: playerId });
+  });
+});
+
 const port = 3001;
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
